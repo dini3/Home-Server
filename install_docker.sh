0e0fcb22a91b600ff29b21c26fc859d50eeef806
dini3 <58658615+dini3@users.noreply.github.com>
2024-02-17 17:31:22 +0800

    Update install_docker.sh
    remove sudo and comments

--- install_docker.sh
+++ install_docker.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
-
-# Install prerequisites
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg lsb-release
-
-# Create directory for Docker keyrings
-sudo mkdir -p /etc/apt/keyrings
-
-# Download and install Docker GPG key
+apt-get update
+apt-get install -y ca-certificates curl gnupg lsb-release
+echo "Certificates installed"
+mkdir -p /etc/apt/keyrings
+echo "Created directory for Docker keyrings"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-# Add Docker repository to APT sources
+echo "Downloaded and install Docker GPG key"
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-# Update APT
-sudo apt-get update
-
-# Install Docker
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-
-# Add current user to the docker group
-sudo groupadd docker
-sudo usermod -aG docker $USER
-
-# Install Docker Compose
-sudo apt-get install docker-compose-plugin
-
+echo "Add Docker repository to APT sources"
+apt-get update
+echo "Update APT"
+apt-get install -y docker-ce docker-ce-cli containerd.io
+echo "Install Docker"
+groupadd docker
+usermod -aG docker $USER
+echo "Add current user to the docker group"
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+echo "Install Docker Compose"
 echo "Docker installation completed."
 echo "Please log out and log back in for the changes to take effect."
